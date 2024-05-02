64595bb3f97a0f3ec2b092f66a37b9d164709a42
dinushan <stdinushan@gmail.com>
2024-05-02 21:11:26 +0530

    do0ne 6

--- lib/services/api_provider.dart
+++ lib/services/api_provider.dart
@@ -1,7 +1,61 @@
-
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class APIProvider {
-  // static const String BASE_URL = "https://us-central1-student-progress-app-854fa.cloudfunctions.net/api";
-  static const String BASE_URL = "http://10.0.2.2:3000";
-  // static const String BASE_URL = "http://192.168.0.2:3000";
+  static const String _baseUrl = 'http://localhost:9000/api/v1';
+  static final Dio _dio = Dio();
+  static APIProvider? _instance;
+
+  String? _token;
+
+  APIProvider._internal();
+
+  factory APIProvider() {
+    if (_instance == null) {
+      _instance = APIProvider._internal();
+      _instance!._initializeInterceptors();
+    }
+    return _instance!;
+  }
+
+  Future<void> _initializeInterceptors() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _token = prefs.getString('token')!;
+
+    _dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) {
+        // Add Authorization header with Bearer token
+        options.headers['Authorization'] = 'Bearer $_token';
+        return handler.next(options);
+      },
+      onResponse: (response, handler) {
+        return handler.next(response);
+      },
+      onError: (DioError e, handler) {
+        return handler.next(e);
+      },
+    ));
+  }
+
+  Future<Response> get(String path) async {
+    try {
+      final response = await _dio.get(_baseUrl + path);
+      return response;
+    } catch (e) {
+      print('Error: $e');
+      throw e;
+    }
+  }
+
+  Future<Response> post(String path, dynamic data) async {
+    try {
+      final response = await _dio.post(_baseUrl + path, data: data);
+      return response;
+    } catch (e) {
+      print('Error: $e');
+      throw e;
+    }
+  }
+
+// Add other HTTP methods as needed
 }
