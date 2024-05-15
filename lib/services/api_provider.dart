9df4ea272ad15db00448450e93f35549e51f0ece
dinushanw <dinushanw@irononetech.com>
2024-05-15 16:32:44 +0530

    Admin Side Implementation

--- lib/services/api_provider.dart
+++ lib/services/api_provider.dart
@@ -2,8 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class APIProvider {
-  static const String _baseUrl = 'http://13.251.19.56:9000/api/v1';
-  static const String BASE_URL = 'http://13.251.19.56:9000/api/v1';
+  static const String _baseUrl = 'http://3.1.1.79:9000/api/v1';
+  static const String BASE_URL = 'http://3.1.1.79:9000/api/v1';
   static Dio? dio;
   static APIProvider? _apiProvider;
   int _maxRetry = 3;
